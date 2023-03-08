ae2ae26c099db705683477e2de3eec53ec881f14
hnwj <2948662591@qq.com>
2023-03-08 20:27:00 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -1,50 +1,78 @@
 import App from './App'
+// 按需导入 $http 对象
+import {
+	$http
+} from '@escook/request-miniprogram'
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 配置基础路径
+$http.baseUrl = 'https://www.uinav.com'
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
 
+// 封装提示方法
+$http.showMsg = function(title = "数据加载失败！", duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 
 try {
-  function isPromise(obj) {
-    return (
-      !!obj &&
-      (typeof obj === "object" || typeof obj === "function") &&
-      typeof obj.then === "function"
-    );
-  }
+	function isPromise(obj) {
+		return (
+			!!obj &&
+			(typeof obj === "object" || typeof obj === "function") &&
+			typeof obj.then === "function"
+		);
+	}
 
-  // 统一 vue2 API Promise 化返回格式与 vue3 保持一致
-  uni.addInterceptor({
-    returnValue(res) {
-      if (!isPromise(res)) {
-        return res;
-      }
-      return new Promise((resolve, reject) => {
-        res.then((res) => {
-          if (res[0]) {
-            reject(res[0]);
-          } else {
-            resolve(res[1]);
-          }
-        });
-      });
-    },
-  });
-} catch (error) { }
+	// 统一 vue2 API Promise 化返回格式与 vue3 保持一致
+	uni.addInterceptor({
+		returnValue(res) {
+			if (!isPromise(res)) {
+				return res;
+			}
+			return new Promise((resolve, reject) => {
+				res.then((res) => {
+					if (res[0]) {
+						reject(res[0]);
+					} else {
+						resolve(res[1]);
+					}
+				});
+			});
+		},
+	});
+} catch (error) {}
 
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
